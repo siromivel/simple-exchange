32f6a3cd4b6c23fee64128a53dff89d261364df8
Levi Morris <siromivel@gmail.com>
2019-04-01 17:29:33 -0600

    Add basic component for portfolio distribution

--- browser/src/components/App.tsx
+++ browser/src/components/App.tsx
@@ -1,6 +1,9 @@
 import React from "react"
+import { PortfolioChart } from "./PortfolioChart";
 
 export const App = () =>
     <div className="main">
         Sup doge
+
+        <PortfolioChart />
     </div>
