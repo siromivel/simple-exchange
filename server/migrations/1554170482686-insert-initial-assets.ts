fdb1a61610a47722e89c47887ca8b636857ce1a1
Levi Morris <siromivel@gmail.com>
2019-04-01 20:29:00 -0600

    Create trading_pair table
    - Also adds initial inserts for trading_pair and asset tables
    - trading_pair down migration could be _much_ cleaner, but not worth the time immediately

---
+++ server/migrations/1554170482686-insert-initial-assets.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class InsertInitialAssets1554170482686 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(
+        `INSERT INTO asset (name, symbol)
+            VALUES ('Bitcoin', 'BTC')`
+    )
+    await queryRunner.query(
+        `INSERT INTO asset (name, symbol)
+            VALUES ('Dogecoin', 'DOGE')`
+    )
+    await queryRunner.query(
+        `INSERT INTO asset (name, symbol)
+            VALUES ('Litecoin', 'LTC')`
+    )
+    await queryRunner.query(
+        `INSERT INTO asset (name, symbol)
+            VALUES ('US Dollar', 'USD')`
+    )
+    await queryRunner.query(
+        `INSERT INTO asset (name, symbol)
+            VALUES ('Monero', 'XMR')`
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(
+      `DELETE FROM asset
+        WHERE symbol = 'BTC'
+           OR symbol = 'DOGE'
+           OR symbol = 'LTC'
+           OR symbol = 'USD'
+           OR symbol = 'XMR'`,
+    )
+  }
+}
---
+++ server/migrations/1554170877809-create-trading-pair-table.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreatePairTable1554170877809 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(
+      `CREATE TABLE "trading_pair" (
+          id serial PRIMARY KEY NOT NULL,
+          base_currency_id integer NOT NULL REFERENCES asset,
+          to_currency_id integer NOT NULL REFERENCES asset,
+          UNIQUE (base_currency_id, to_currency_id)
+      )`,
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(
+      `DROP TABLE "trading_pair"`,
+    )
+  }
+}
