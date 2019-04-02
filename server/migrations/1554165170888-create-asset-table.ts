ff9152688777d1f8e69d5f4fc3dbbe11d56483b9
Levi Morris <siromivel@gmail.com>
2019-04-01 18:43:09 -0600

    Create holding table migration
    - Updated user => exchange_user, this is due "user" being a reserved keyword in Postgres
    - Added primary keys to all tables

--- server/migrations/1554165170888-create-asset-table.ts
+++ server/migrations/1554165170888-create-asset-table.ts
@@ -4,7 +4,7 @@ export class CreateAssetTable1554165170888 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(
       `CREATE TABLE asset (
-          id serial UNIQUE NOT NULL,
+          id serial PRIMARY KEY NOT NULL,
           name text NOT NULL,
           symbol text UNIQUE NOT NULL
       )`,
--- server/migrations/1554165374083-create-holding-table.ts
+++ server/migrations/1554165374083-create-holding-table.ts
@@ -1,18 +1,20 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreateUserTable1554164532144 implements MigrationInterface {
+export class CreateHoldingTable1554165374083 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(
-      `CREATE TABLE exchange_user (
+      `CREATE TABLE holding (
           id serial PRIMARY KEY NOT NULL,
-          name text UNIQUE NOT NULL
+          balance decimal NOT NULL DEFAULT 0,
+          currency_id integer NOT NULL REFERENCES asset,
+          user_id integer NOT NULL REFERENCES exchange_user
       )`,
     )
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(
-      `DROP TABLE exchange_user`,
+      `DROP TABLE holding`,
     )
   }
 }
