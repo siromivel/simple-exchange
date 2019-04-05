f7ddbe895d2d6c837ee5dec9393ee477cf470381
Levi Morris <siromivel@gmail.com>
2019-04-05 08:16:33 -0600

    Add check constraints to Trade

--- server/migrations/1554301961181-create-trade-table.ts
+++ server/migrations/1554301961181-create-trade-table.ts
@@ -6,10 +6,11 @@ export class CreateTradeTable1554301961181 implements MigrationInterface {
       `CREATE TABLE trade (
           id uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
           type text NOT NULL,
-          quantity decimal NOT NULL,
-          price decimal NOT NULL,
+          quantity decimal NOT NULL CHECK (quantity > 0),
+          price decimal NOT NULL CHECK (price > 0),
           trading_pair_id integer NOT NULL REFERENCES trading_pair,
-          exchange_user_id integer NOT NULL REFERENCES exchange_user
+          exchange_user_id integer NOT NULL REFERENCES exchange_user,
+          executed timestamp NOT NULL DEFAULT now()
       )`,
     )
   }
